55cc1b041e65ce26b6653ba3eb89d9c55e0e146d
kabya <kabya78890@gmail.com>
2025-07-05 12:39:50 -0400

    update

--- new_startup/src/app/navbar/navbar.component.ts
+++ new_startup/src/app/navbar/navbar.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { HeroSectionComponent } from '../hero-section/hero-section.component';
-import { AboutUsComponent } from '../component/about-us/about-us.component';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [HeroSectionComponent, AboutUsComponent],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
