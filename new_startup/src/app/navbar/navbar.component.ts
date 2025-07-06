cdeff59a0fd5902e3a3b7baffc936451e6696260
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2025-07-06 22:17:03 +0530

    update

--- new_startup/src/app/navbar/navbar.component.ts
+++ new_startup/src/app/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+// import { HeroSectionComponent } from '../hero-section/hero-section.component';
+
 
 
 @Component({
   selector: 'app-navbar',
+
   imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
