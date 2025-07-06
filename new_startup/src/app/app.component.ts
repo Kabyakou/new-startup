e3692690c58eaf2256f9c9f729c9df2745de486e
kabya <kabya78890@gmail.com>
2025-07-06 12:15:51 -0400

    update

--- new_startup/src/app/app.component.ts
+++ new_startup/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent,RouterOutlet,HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
