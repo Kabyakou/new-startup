59bfeca4985f193e05ad9ee7de03a025eafc078c
kabya <kabya78890@gmail.com>
2025-07-07 12:39:55 -0400

    delete_data

--- new_startup/src/app/app.component.ts
+++ new_startup/src/app/app.component.ts
@@ -6,24 +6,7 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> 5ee9bced82c3854f8c20c9976d6eed1fb77cd60b
-
-
   imports: [NavbarComponent,RouterOutlet,HeroSectionComponent],
-
-<<<<<<< HEAD
-=======
-  imports: [RouterOutlet, NavbarComponent],
->>>>>>> Stashed changes
-=======
->>>>>>> 5ee9bced82c3854f8c20c9976d6eed1fb77cd60b
-=======
-  imports: [RouterOutlet, NavbarComponent],
->>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
