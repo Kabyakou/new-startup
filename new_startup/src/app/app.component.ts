86caf141df9252e1f7b53f8d4b745a7d71f0c381
kabya <kabya78890@gmail.com>
2025-07-07 12:23:50 -0400

    update

--- new_startup/src/app/app.component.ts
+++ new_startup/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 <<<<<<< Updated upstream
 =======
@@ -20,6 +21,9 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 >>>>>>> Stashed changes
 =======
 >>>>>>> 5ee9bced82c3854f8c20c9976d6eed1fb77cd60b
+=======
+  imports: [RouterOutlet, NavbarComponent],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
